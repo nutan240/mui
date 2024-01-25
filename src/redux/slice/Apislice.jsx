10588385f970510f92f3f6c97aba0e3c6cd70fcb
nutan kumari <nutananju240@gmail.com>
2024-01-25 18:48:43 +0530

    create axios instance

--- src/redux/slice/Apislice.jsx
+++ src/redux/slice/Apislice.jsx
@@ -1,34 +1,28 @@
-
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
- export const fetchapi = createAsyncThunk("fetchapi", async () => {
-  const response = await axios("https://jsonplaceholder.typicode.com/users");
-  return response.data;
-});
+
 const apislice = createSlice({
   name: "api",
   initialState: {
-    isLoading:false,
-    data:[],
-    isError:false,
+    data: [],
   },
-
-  extraReducers: (builder) => {
-    builder.addCase(fetchapi.pending, (state, action) => {
-      state.isLoading = true;
-      console.log("pending" );
-    });
-
-    builder.addCase(fetchapi.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.data = action.payload;
-      console.log("fulfilled" ,action.payload)
-    });
-    builder.addCase(fetchapi.rejected, (state, action) => {
-        console.log("error" ,action.payload);
-        state.isError=true;
-      });
+  reducers: {
+    addData: (state, action) => {
+      return { ...state, data: action.payload };
+    },
   },
 });
 
+export const fetchData = () => async (dispatch) => {
+  try {
+    let response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    dispatch(addData(response.data));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const { addData } = apislice.actions;
 export default apislice.reducer;
